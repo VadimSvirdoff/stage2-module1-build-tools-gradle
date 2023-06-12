8b2461a84ddba4056003c560ee686120ad2c534c
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-12 12:17:41 +0300

    add gradle settings

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,26 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        if (args == null || args.isEmpty()) {
+            return false;
+        }
+
+        for (String str : args) {
+            if (str == null || str.isEmpty()) {
+                return false;
+            }
+
+            for (char c : str.toCharArray()) {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+
+            int number = Integer.parseInt(str);
+            return number > 0;
+        }
+
+        return true;
+
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,17 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        int number = Integer.parseInt(str);
+        return number > 0;
     }
 }
